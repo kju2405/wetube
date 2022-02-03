877202ccc8c1e4292c7d72f5e78cdfd48668f280
woong <kju2405@khu.ac.kr>
2022-02-03 12:15:30 +0900

    Using view engine - pug

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,5 +1,5 @@
-export const trending=(req,res)=>res.send("Home Page Videos");
-export const watch=(req,res)=>res.send("Watch Video");
+export const trending=(req,res)=>res.render("home");
+export const watch=(req,res)=>res.render("watch");
 export const edit=(req,res)=>res.send("Edit Video");
 export const search=(req,res)=>res.send("Search Video");
 export const upload=(req,res)=>res.send("Upload Video");
