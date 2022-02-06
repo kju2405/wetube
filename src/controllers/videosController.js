f56e927f5a254ad7daca977aaa1e47ae4c1ea242
woong <kju2405@khu.ac.kr>
2022-02-06 16:04:37 +0900

    Making form - post

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -30,9 +30,27 @@ export const trending = (req, res) => {
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  console.log(req.body);
+  return res.redirect("/");
+};
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
